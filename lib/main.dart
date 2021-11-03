faa36d05027d7aa7245f1412ce8d793848dc043c
carissafarry <69377985+carissafarry@users.noreply.github.com>
2021-11-03 12:08:20 +0700

    #9 Buttons & Icons

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,43 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.brown[200],
       ),
       body: Center(
+        child: IconButton(
+          onPressed: () {
+            print('you clicked me!');
+          }, 
+          icon: Icon(
+            Icons.alternate_email
+          ),
+          color: Colors.deepOrangeAccent[100],
+        ),
+
+        // child: RaisedButton.icon(
+        //   onPressed: () {},
+        //   icon: Icon(Icons.mail),
+        //   label: Text('mail me'),
+        //   color: Colors.amber,
+        // )
+
+        // child: Icon(
+        //   Icons.airport_shuttle,
+        //   color: Colors.lightBlue,
+        //   size: 50.0,
+        // ),
+
         // child: Image(
         //   image: AssetImage('assets/p1.jfif'),
         // ),
-        child: Image.asset('assets/p1.jfif'),
+
+        // child: Image.asset('assets/p1.jfif'),
+
         // child:  Image.network('https://images.unsplash.com/photo-1635241903012-7b313beb81fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click'),
+        child: Text(
+          '+',
+          textAlign: TextAlign.center,
+        ),
         backgroundColor: Colors.blueGrey[700],
       ),
     );
